29d9f0a105ddae5b8385863ce97da953fa33175a
NoNamePoint <krassergey-37@mail.ru>
2020-09-26 17:10:33 +0300

    add models data into tmplts

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+from .models import *
 
 
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_orders = orders.count()
+    total_customers = customers.count()
+    delivered = orders.filter(status='Доставлено').count()
+    total_pendings = orders.filter(status='Ожидание').count()
+
+    context = {'orders': orders, 'customers': customers, "total_customers": total_customers,
+               'total_orders': total_orders, 'total_pendings': total_pendings, 'delivered': delivered}
+
+    return render(request, 'accounts/dashboard.html', context=context)
 
 
 def products(request):
-	return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-	return render(request, 'accounts/customer.html')
+def customer(request, primary_key):
+    customers = Customer.objects.get(id=primary_key)
+    order = customers.order_set.all()
+    context = {'customers': customers, 'order': order}
+    return render(request, 'accounts/customer.html', context=context)
+	
+
+
+
